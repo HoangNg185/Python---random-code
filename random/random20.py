9ec0dd203a8f43aa2c30e7ee30c3f5b044c34926
Liam Nguyen <longlong1852@gmail.com>
2024-02-22 01:43:16 -0500

    a

---
+++ random/random20.py
@@ -0,0 +1,11 @@
+#what is missing in this array?
+nums =[0,1,3,5,8,10,30,15]
+
+missing = []
+start = 0
+
+while start<= max(nums):
+    if start not in nums:
+        missing.append(start)
+    start +=1
+print(missing)
---
+++ random/random21.py
@@ -0,0 +1,15 @@
+# Between these 2 numbers, what are prime numbers?
+lower= 90
+upper=250
+
+prime=[]
+increase=2
+for i in range(lower,upper+1):
+    for j in range(2,i):
+        if i%j==0:
+            break
+    else:
+        prime.append(i)
+print(f'Prime numbers between {lower} and {upper} are: \n{prime}')
+
+
