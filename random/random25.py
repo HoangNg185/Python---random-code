280dde43b3285bfcc9fef85907c12e4fb7f24c0f
Liam Nguyen <longlong1852@gmail.com>
2024-02-24 02:53:17 -0500

    this is my commit message from python file

--- random/random25.py
+++ random/random25.py
@@ -2,17 +2,10 @@ import pandas as pd
 
 df =pd.read_excel('Data manipulation.xlsx')
 
-prime=[]
-for i in range(min(df['No']),max(df['No'])+1):
-    for j in range(2,i):
-        if i%j==0:
-            break
-    else:
-        prime.append(i)
+df['Right cell'] = ' '
 
-for right_cell in range(0, max(df['No'])):
-    df.loc[right_cell, 'Prime no']= 'asd'
 
+for i in df['Right cell'].index:
+    df.loc[i,'Right cell']
 print(df.to_string())
-print(prime)
 
