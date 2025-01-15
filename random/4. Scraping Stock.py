d2a6953df1c33e2d1862168fdfc020fd104a8655
Liam Nguyen <longlong1852@gmail.com>
2025-01-15 01:19:51 -0500

    Some changes were made

--- random/4. Scraping Stock.py
+++ random/4. Scraping Stock.py
@@ -15,7 +15,7 @@ chrome_options.add_argument("--headless")
 service = Service("C:\\Users\\Liam\\.cache\\selenium\\chromedriver\\win64\\131\\chromedriver.exe")
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-df = pd.read_csv('C:\\Users\\Liam\\OneDrive - Seneca\\Desktop\\1. Me\\stock_mysql.csv', index_col='ID')
+df = pd.read_csv('Outputs\\stock_mysql.csv', index_col='ID')
 today = strftime('%h %d, %H:%M')
 df[today] = ''
 # df=df.drop(columns=['stock_name','date_added','category','capital_value_at_added_date','relationships_and_notes'])
@@ -26,7 +26,7 @@ for i in df.index:
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
     response = requests.get(url, headers=headers)
-    time.sleep(2)
+    time.sleep(5)
     soup = BeautifulSoup(response.text, "html.parser")
 
     percentage = soup.find('span', {'jsname': 'rfaVEf'})
@@ -44,7 +44,7 @@ for i in df.index:
         print(f'{df.loc[i, "search_symbol"]}:{df.loc[i, today]}')
 
 print(df.to_string())
-df.to_csv('C:\\Users\\Liam\\OneDrive - Seneca\\Desktop\\1. Me\\stock_mysql.csv')
+df.to_csv('Outputs\\stock_mysql.csv')
 end_time = time.time()
 elapsed_time = end_time - start_time
 print(f'Total time taken:{elapsed_time:.2f} seconds')
